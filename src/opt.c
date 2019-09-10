ed96d6163827eeb2357c38457b464e4c6cc2342b
Warren Toomey <wkt@tuhs.org>
2019-09-10 14:16:09 +1000

    Comments for opt.c

--- src/opt.c
+++ src/opt.c
@@ -10,6 +10,11 @@
 
 static int	Opt_sum_lim;
 
+// Calculate the maximum value for an addition.
+// Be careful not to exceed the compiler's internal
+// limits, in case we are acting as a cross-compiler
+// for a target with a bigger word size. Store the
+// final value in Opt_sum_lim.
 void opt_init(void) {
 	int	i, k;
 
@@ -20,12 +25,20 @@ void opt_init(void) {
 		Opt_sum_lim <<= 1;
 }
 
+// Fold an AST tree with a binary operator
+// and two INTLIT children. Return either 
+// the original tree or a new leaf node.
 static node *fold2(node *n) {
 	int	v, vl, vr;
 
+	// Get the values from each child
 	vl = n->left->args[0];
 	vr = n->right->args[0];
 	switch (n->op) {
+
+	// For addition and subtraction, ensure we don't
+	// overflow our internal int variables. If we do,
+	// return the original tree
 	case OP_PLUS:
 	case OP_ADD:	if (abs(vl) >= Opt_sum_lim || abs(vr) >= Opt_sum_lim)
 				return n;
@@ -35,10 +48,17 @@ static node *fold2(node *n) {
 				return n;
 			v = vl - vr;
 			break;
+
+	// Sanity check. Do the multiplication followed by division.
+	// Check we get the original result back. If not, return
+	// the original tree
 	case OP_MUL:	v = vl * vr;
 			if (v / vl != vr) return n;
 			if ((v & (Opt_sum_lim-1)) != v) return n;
 			break;
+
+	// Don't permit a divide by zero. Let the
+	// resulting executable deal with it!
 	case OP_DIV:	if (0 == vr) return n;
 			v = vl / vr;
 			break;
@@ -48,12 +68,17 @@ static node *fold2(node *n) {
 	case OP_BINAND:	v = vl & vr; break;
 	case OP_BINIOR:	v = vl | vr; break;
 	case OP_BINXOR:	v = vl ^ vr; break;
+
+	// Don't permit shifts that are too big
 	case OP_LSHIFT:	if (vr > BPW*8-1) return n;
 			v = vl << vr;
 			break;
 	case OP_RSHIFT:	if (vr > BPW*8-1) return n;
 			v = vl >> vr;
 			break;
+
+	// Calculate a true/false (1/0) value
+	// for comparisons
 	case OP_EQUAL:	v = vl == vr; break;
 	case OP_NOTEQ:	v = vl != vr; break;
 	case OP_LESS:	v = vl <  vr; break;
@@ -63,12 +88,20 @@ static node *fold2(node *n) {
 	default:	return n;
 			break;
 	}
+
+	// Make an OP_LIT leaf node with new value
 	return mkleaf(OP_LIT, v);
 }
 
+// Fold an AST tree with a unary operator
+// and one INTLIT children. Return either 
+// the original tree or a new leaf node.
 static node *fold1(node *n) {
 	int	v;
 
+	// Get the child value. Do the
+	// operation if recognised.
+	// Return the new leaf node.
 	v = n->left->args[0];
 	switch (n->op) {
 	case OP_NOT:	v = ~v; break;
@@ -76,19 +109,24 @@ static node *fold1(node *n) {
 	case OP_NEG:	v = -v; break;
 	case OP_SCALE:	v = v * BPW; break;
 	default:	return n;
-			break;
+			break;	// XXX: why?
 	}
 	return mkleaf(OP_LIT, v);
 }
 
+// Attempt to reduce an AST tree
+// with the root node n
 static node *reduce(node *n) {
 	int	k, i, lim, op, cl, cr, vl = 0, vr = 0;
 
+	// Get the operation and any literal child values
 	op = n->op;
 	cl = OP_LIT == n->left->op;
 	cr = OP_LIT == n->right->op;
 	if (cl) vl = n->left->args[0];
 	if (cr) vr = n->right->args[0];
+
+	// Optimise away operations involving zero
 	if ((OP_PLUS == op || OP_ADD == op) && cr && 0 == vr)	/* x+0 -> x */
 		return n->left;
 	if ((OP_PLUS == op || OP_ADD == op) && cl && 0 == vl)	/* 0+x -> x */
@@ -101,10 +139,20 @@ static node *reduce(node *n) {
 		return mkleaf(OP_LIT, 0);
 	if (OP_MUL == op && cr && 0 == vr)			/* x*0 -> 0 */
 		return mkleaf(OP_LIT, 0);
+
+	// Look for multiply/divide operations
+	// where one value is a power of two
 	if (OP_MUL == op || OP_DIV == op) {
 		lim = BPW * 8 - 1;
+
+		// By looping across each power of two
+		// from the least to most significant
+		// bit in the target word size
 		for (k=1,i=0; i<lim; i++, k<<=1) {
+
+			// vr is a power of two
 			if (cr && k == vr) {
+				// So replace the operation with a bit shift
 				if (OP_MUL == op)
 					return mkbinop(OP_LSHIFT, n->left,
 							mkleaf(OP_LIT, i));
@@ -112,7 +160,9 @@ static node *reduce(node *n) {
 					return mkbinop(OP_RSHIFT, n->left,
 							mkleaf(OP_LIT, i));
 			}
+			// vl is a power of two
 			else if (cl && k == vl && OP_MUL == op) {
+				// So replace the operation with a bit shift
 				return mkbinop(OP_LSHIFT, n->right,
 						mkleaf(OP_LIT, i));
 			}
@@ -121,17 +171,28 @@ static node *reduce(node *n) {
 	return n;
 }
 
+// Attempt to fold and reduce an
+// AST tree with the root node n
 node *fold_reduce(node *n) {
 	if (NULL == n) return NULL;
+
+	// Fold & reduce the left child
+	// then do the same on the right child
 	n->left = fold_reduce(n->left);
 	n->right = fold_reduce(n->right);
+
+	// Both children are OP_LITs, do a fold2()
 	if (n->left && OP_LIT == n->left->op &&
 	    n->right && OP_LIT == n->right->op)
 	{
 		n = fold2(n);
 	}
+
+	// The left child is an OP_LIT, do a fold1()
 	else if (n->left && OP_LIT == n->left->op)
 		n = fold1(n);
+
+	// Both children are OP_LITs, do a reduce()
 	if (n->left && n->right &&
 	    (OP_LIT == n->left->op || OP_LIT == n->right->op))
 	{
@@ -140,16 +201,30 @@ node *fold_reduce(node *n) {
 	return n;
 }
 
+// Try to reorder some of the
+// operations in an AST tree
 node *reorder_ops(node *n) {
 	int	op, lop, t;
 	node	*tn;
 
 	if (NULL == n) return NULL;
+
+	// Reorder any operations on the left,
+	// then on the right
 	n->left = reorder_ops(n->left);
 	n->right = reorder_ops(n->right);
+
+	// If nothing got done, return the original tree
 	if (NULL == n->right || NULL == n->left) return n;
+
+
 	op = n->op;
 	lop = n->left->op;
+
+	// Conditions to meet:
+	// 1. Either the left child is a literal, or
+	//    an rvalue which is an identifier
+	// 2. The operation is arithmetic or bitwise.
 	if (	(OP_LIT == lop ||
 		 (OP_RVAL == lop &&
 		  n->left->left &&
@@ -159,11 +234,13 @@ node *reorder_ops(node *n) {
 		 OP_BINAND == op || OP_BINIOR == op || OP_BINXOR == op ||
 		 OP_EQUAL == op || OP_NOTEQ == op)
 	) {
-		if (OP_PLUS == op) {	/* switch types */
+		if (OP_PLUS == op) {	/* switch types */	// XXX: Why?
 			t = n->args[0];
 			n->args[0] = n->args[1];
 			n->args[1] = t;
 		}
+
+		// Swap the child nodes around			// XXX: Why?
 		tn = n->right;
 		n->right = n->left;
 		n->left = tn;
@@ -171,6 +248,10 @@ node *reorder_ops(node *n) {
 	return n;
 }
 
+// Optimise an AST tree by
+// folding and reducing all
+// sub-trees, then reordering
+// any operations where possible
 node *optimize(node *n) {
 	n = fold_reduce(n);
 	n = reorder_ops(n);
